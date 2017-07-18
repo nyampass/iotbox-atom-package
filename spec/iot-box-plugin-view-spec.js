e6cd91cdcfaf8f83dceff382549ce7364dfa0771
Tokusei Noborio <tokusei@nyampass.com>
2017-07-19 07:50:13 +0900

    First commit

---
+++ spec/iot-box-plugin-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import IotBoxPluginView from '../lib/iot-box-plugin-view';
+
+describe('IotBoxPluginView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});

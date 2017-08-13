6888fd8cbfa5752553605fd2d4edb49e1421d054
Tokusei Noborio <tokusei@nyampass.com>
2017-08-13 17:38:55 +0900

    Rename package name to iotbox

--- spec/iot-box-plugin-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import IotBoxPluginView from '../lib/iot-box-plugin-view';
-
-describe('IotBoxPluginView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
